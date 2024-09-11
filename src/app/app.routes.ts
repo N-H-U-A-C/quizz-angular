2aea252e2145f89f904dd6e160e6ec6f7bd7ab32
Margaux Doisy <margauxdoisy@gmail.com>
2024-09-11 13:08:21 +0200

    header+footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,13 @@
 import {Routes} from '@angular/router';
 import {HomeComponent} from "./pages/home/home.component";
+import {QuizzComponent} from "./pages/quizz/quizz.component";
+import {QuestionComponent} from "./pages/question/question.component";
+import {ResultComponent} from "./pages/result/result.component";
 
 export const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', redirectTo: ''},
+  {path: 'quizz', component: QuizzComponent},
+  {path: 'questions', component: QuestionComponent},
+  {path: 'reponses', component: ResultComponent},
 ];
